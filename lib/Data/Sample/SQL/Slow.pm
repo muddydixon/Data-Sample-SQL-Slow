3082721a0fd04aee225bb14e6410d7329517ecd2
muddydixon <muddydixon@gmail.com>
2013-05-06 03:42:55 +0900

    init

---
+++ lib/Data/Sample/SQL/Slow.pm
@@ -0,0 +1,39 @@
+package Data::Sample::SQL::Slow;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Sample::SQL::Slow - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::Sample::SQL::Slow;
+
+=head1 DESCRIPTION
+
+Data::Sample::SQL::Slow is ...
+
+=head1 LICENSE
+
+Copyright (C) muddydixon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+muddydixon E<lt>muddydixon@gmail.comE<gt>
+
+=cut
+
